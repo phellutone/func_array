f7d811e440be3a0b1ff62f6cc862608fb9028081
phellutone <phellutone@gmail.com>
2021-12-14 14:22:57 +0900

    Create func_array.py

---
+++ func_array.py
@@ -0,0 +1,66 @@
+import bpy
+from .func_array_variable import FuncArrayVariable
+
+class FuncArray(bpy.types.PropertyGroup):
+    name: bpy.props.StringProperty()
+    mute: bpy.props.BoolProperty()
+
+    target: bpy.props.PointerProperty(
+        type=bpy.types.Object,
+        poll=lambda self, object: object.type == 'MESH'
+    )
+    eval_target: bpy.props.PointerProperty(
+        type=bpy.types.Object,
+        poll=lambda self, object: object.type == 'MESH'
+    )
+
+    count: bpy.props.IntProperty(
+        min=1,
+        default=1,
+        soft_max=25
+    )
+
+    variables: bpy.props.CollectionProperty(type=FuncArrayVariable)
+    active_variable_index: bpy.props.IntProperty()
+
+class FUNCARRAY_OT_add(bpy.types.Operator):
+    bl_idname = 'func_array.add'
+    bl_label = 'add'
+    bl_description = ''
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        farray: list[FuncArray] = context.scene.func_array
+        block: FuncArray = farray.add()
+
+        block.name = 'Array '+str(len(farray))
+        context.scene.active_func_array_index = len(farray)-1
+        return {'FINISHED'}
+
+class FUNCARRAY_OT_remove(bpy.types.Operator):
+    bl_idname = 'func_array.remove'
+    bl_label = 'remove'
+    bl_description = ''
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        farray: list[FuncArray] = context.scene.func_array
+        index: int = context.scene.active_func_array_index
+        if index < 0 or not farray:
+            return {'CANCELLED'}
+
+        block: FuncArray = farray[index]
+
+        farray.remove(index)
+        context.scene.active_func_array_index = min(max(0, index), len(farray)-1)
+        return {'FINISHED'}
+
+class OBJECT_UL_FuncArray(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            row = layout.row(align=True)
+            row.prop(item, 'name', icon='ANIM', text='', emboss=False)
+            row.prop(item, 'mute', icon='CHECKBOX_HLT' if not item.mute else 'CHECKBOX_DEHLT', text='', emboss=False)
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label(text='', icon=icon)
