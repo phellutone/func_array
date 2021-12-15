8cc5d63db5a770e6481111d992a3c8ad590062bd
phellutone <phellutone@gmail.com>
2021-12-16 08:50:55 +0900

    Update func_array.py

--- func_array.py
+++ func_array.py
@@ -5,6 +5,7 @@ class FuncArray(bpy.types.PropertyGroup):
     index: bpy.props.IntProperty()
     name: bpy.props.StringProperty()
     mute: bpy.props.BoolProperty()
+    is_activate: bpy.props.BoolProperty()
 
     target: bpy.props.PointerProperty(
         type=bpy.types.Object,
@@ -65,6 +66,39 @@ class FUNCARRAY_OT_remove(bpy.types.Operator):
         context.scene.active_func_array_index = min(max(0, index), len(farray)-1)
         return {'FINISHED'}
 
+class FUNCARRAY_OT_activate(bpy.types.Operator):
+    bl_idname = 'func_array.activate'
+    bl_label = 'activate'
+    bl_description = ''
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        farray: list[FuncArray] = context.scene.func_array
+        index: int = context.scene.active_func_array_index
+        if index < 0 or not farray:
+            return {'CANCELLED'}
+
+        block: FuncArray = farray[index]
+        if block.is_activate:
+            return {'CANCELLED'}
+        if block.target is None:
+            return {'CANCELLED'}
+        
+        me = bpy.data.meshes.new('FuncArrayDummy.'+block.target.name)
+        ob = bpy.data.objects.new('FuncArrayDummy.'+block.target.name, me)
+        block.eval_target = ob
+
+        co = bpy.data.collections.new('FuncArrayDummy.'+block.target.name)
+        co.objects.link(ob)
+
+        obj = bpy.data.objects.new('FuncArray.'+block.target.name, None)
+        obj.instance_type = 'COLLECTION'
+        obj.instance_collection = co
+        context.scene.collection.objects.link(obj)
+
+        block.is_activate = True
+        return {'FINISHED'}
+
 class OBJECT_UL_FuncArray(bpy.types.UIList):
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
         if self.layout_type in {'DEFAULT', 'COMPACT'}:
@@ -76,11 +110,11 @@ class OBJECT_UL_FuncArray(bpy.types.UIList):
             layout.label(text='', icon=icon)
 
 class OBJECT_PT_FuncArray(bpy.types.Panel):
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    bl_category = "Tool"
-    bl_idname = "VIEW3D_PT_func_array"
-    bl_label = "Func Array"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
+    bl_idname = 'VIEW3D_PT_func_array'
+    bl_label = 'Func Array'
     bl_options = {'DEFAULT_CLOSED'}
     
     def draw(self, context):
@@ -88,24 +122,26 @@ class OBJECT_PT_FuncArray(bpy.types.Panel):
         layout = self.layout
 
         row = layout.row()
-        row.template_list("OBJECT_UL_FuncArray", "", scene, "func_array", scene, "active_func_array_index", rows=3)
+        row.template_list('OBJECT_UL_FuncArray', '', scene, 'func_array', scene, 'active_func_array_index', rows=3)
         col = row.column(align=True)
-        col.operator("func_array.add", icon="ADD", text="")
-        col.operator("func_array.remove", icon="REMOVE", text="")
+        col.operator('func_array.add', icon='ADD', text='')
+        col.operator('func_array.remove', icon='REMOVE', text='')
 
         if scene.func_array:
             index = scene.active_func_array_index
             block = scene.func_array[index]
 
             col = layout.column()
-            col.prop(block, "target", text="Target")
-            col.prop(block, "count", text="Count")
+            col.prop(block, 'target', text='Target')
+            col.prop(block, 'count', text='Count')
+            col.operator('func_array.activate')
 
 
 classes = (
     FuncArray,
     FUNCARRAY_OT_add,
     FUNCARRAY_OT_remove,
+    FUNCARRAY_OT_activate,
     OBJECT_UL_FuncArray,
     OBJECT_PT_FuncArray
 )
