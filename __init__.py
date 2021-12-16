98411352ce9df57882dc81a4f891950a1bb85f7a
phellutone <phellutone@gmail.com>
2021-12-16 17:51:06 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,41 +11,10 @@ bl_info = {
     'category': 'Object'
 }
 
-
-if 'bpy' in locals():
-    import imp
-    imp.reload(func_array)
-    imp.reload(func_array_variable)
-else:
-    from .func_array import FuncArray, FUNCARRAY_OT_add, FUNCARRAY_OT_remove, FUNCARRAY_OT_activate, OBJECT_UL_FuncArray, OBJECT_PT_FuncArray
-    from .func_array_variable import FuncArrayVariable, FUNCARRAY_OT_variable_add, FUNCARRAY_OT_variable_remove, OBJECT_UL_FuncArrayVariable, OBJECT_PT_FuncArrayVariable
-
-import bpy
-
-classes = (
-    FuncArrayVariable,
-    FuncArray,
-    FUNCARRAY_OT_add,
-    FUNCARRAY_OT_remove,
-    FUNCARRAY_OT_activate,
-    FUNCARRAY_OT_variable_add,
-    FUNCARRAY_OT_variable_remove,
-    OBJECT_UL_FuncArray,
-    OBJECT_UL_FuncArrayVariable,
-    OBJECT_PT_FuncArray,
-    OBJECT_PT_FuncArrayVariable
-)
+from . import func_array
 
 def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
-    
-    bpy.types.Scene.func_array = bpy.props.CollectionProperty(type=FuncArray)
-    bpy.types.Scene.active_func_array_index = bpy.props.IntProperty()
+    func_array.register()
 
 def unregister():
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
-    
-    del bpy.types.Scene.func_array
-    del bpy.types.Scene.active_func_array_index
+    func_array.unregister()
