b53e5bee081e3af500b99656ab9edde046b4eda0
phellutone <phellutone@gmail.com>
2021-12-21 08:18:39 +0900

    Update func_array.py

--- func_array.py
+++ func_array.py
@@ -1,4 +1,5 @@
 import bpy
+import bmesh
 from .handler import deform_update
 
 
@@ -122,7 +123,28 @@ class FUNCARRAY_OT_activation(bpy.types.Operator):
             if block.mute:
                 continue
 
-            print('execution')
+            eval_obj_init(block, block.count, block.trg_co)
+
+            ob = block.target.copy()
+            context.scene.collection.objects.link(ob)
+            for i in range(block.count):
+                eval_mesh = block.eval_targets[i].object.data
+                bm = bmesh.new()
+                bm.from_mesh(eval_mesh)
+                bm.clear()
+
+                block.controller = i/(block.count-1)
+                depsgraph = context.evaluated_depsgraph_get()
+                eval_obj = ob.evaluated_get(depsgraph)
+                me = bpy.data.meshes.new_from_object(eval_obj)
+                me.transform(eval_obj.matrix_world)
+                bm.from_mesh(me)
+                bpy.data.meshes.remove(me)
+
+                bm.to_mesh(eval_mesh)
+                bm.free()
+            bpy.data.objects.remove(ob)
+
             block.is_evaluated = True
 
         return {'PASS_THROUGH'}
