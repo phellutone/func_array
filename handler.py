f69c08458258dfa3f04f689487cf97eef85fde39
phellutone <phellutone@gmail.com>
2021-12-22 10:49:36 +0900

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,37 +1,50 @@
 import bpy
+import bmesh
+from .properties import FuncArray, FuncArrayObject
 
-FUNCARRAY_UPDATE_LOCK = False
+def eval_obj_init(block: FuncArray, count: int, co: bpy.types.Collection):
+    if len(block.eval_targets) > count:
+        for i in reversed(range(count, len(block.eval_targets))):
+            e = block.eval_targets[i]
+            ob = e.object
+            me = e.object.data
+            bpy.data.objects.remove(ob)
+            bpy.data.meshes.remove(me)
+            block.eval_targets.remove(i)
+    elif len(block.eval_targets) < count:
+        for i in range(count-len(block.eval_targets)):
+            e: FuncArrayObject = block.eval_targets.add()
+            me = bpy.data.meshes.new('FuncArrayDummy.'+block.target.name+'.'+str(i))
+            ob = bpy.data.objects.new('FuncArrayDummy.'+block.target.name+'.'+str(i), me)
+            ob.is_func_array_dummy = True
+            e.object = ob
+            co.objects.link(ob)
 
-@bpy.app.handlers.persistent
-def deform_update(scene, depsgraph):
-    global FUNCARRAY_UPDATE_LOCK
-    if FUNCARRAY_UPDATE_LOCK:
-        return
+def eval_dup(context: bpy.types.Context, block: FuncArray):
+    eval_obj_init(block, block.count, block.trg_co)
 
-    ids = [u.id.original for u in depsgraph.updates if isinstance(u.id, bpy.types.Object)]
-    if not ids:
-        return
-    
-    farray = scene.func_array
-    index = scene.active_func_array_index
-    if not farray or index < 0:
-        return
-    
-    FUNCARRAY_UPDATE_LOCK = True
+    ob = block.target.copy()
+    context.scene.collection.objects.link(ob)
 
-    for block in farray:
-        if not block.is_activate:
-            continue
+    ctr = block.controller
+    for i in range(block.count):
+        eval_mesh = block.eval_targets[i].object.data
+        bm = bmesh.new()
+        bm.from_mesh(eval_mesh)
+        bm.clear()
 
-        if not block.is_evaluated:
-            continue
+        if block.count == 1:
+            block.controller = block.ctr_min
+        else:
+            block.controller = i/(block.count-1)*(block.ctr_max-block.ctr_min)+block.ctr_min
+        depsgraph = context.evaluated_depsgraph_get()
+        eval_obj = ob.evaluated_get(depsgraph)
+        me = bpy.data.meshes.new_from_object(eval_obj)
+        me.transform(eval_obj.matrix_world)
+        bm.from_mesh(me)
+        bpy.data.meshes.remove(me)
 
-        if block.lock:
-            continue
-
-        if not block.target in ids:
-            continue
-
-        block.is_evaluated = False
-
-    FUNCARRAY_UPDATE_LOCK = False
+        bm.to_mesh(eval_mesh)
+        bm.free()
+    bpy.data.objects.remove(ob)
+    block.controller = ctr
