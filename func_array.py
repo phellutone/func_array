4518efe7caccef818b681d705f3d3fd157263fc8
phellutone <phellutone@gmail.com>
2021-12-22 18:32:31 +0900

    Update func_array.py

--- func_array.py
+++ func_array.py
@@ -2,6 +2,7 @@ import bpy
 from .properties import FuncArray, FuncArrayObject
 from .operators import FUNCARRAY_OT_add, FUNCARRAY_OT_remove, FUNCARRAY_OT_activation
 from .panels import OBJECT_UL_FuncArray, OBJECT_PT_FuncArray
+from .handlers import deform_update
 
 classes = (
     FuncArrayObject,
@@ -21,6 +22,9 @@ def register():
     bpy.types.Scene.active_func_array_index = bpy.props.IntProperty()
     bpy.types.Object.is_func_array_dummy = bpy.props.BoolProperty()
 
+    if not deform_update in bpy.app.handlers.depsgraph_update_post:
+        bpy.app.handlers.depsgraph_update_post.append(deform_update)
+
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
@@ -28,3 +32,6 @@ def unregister():
     del bpy.types.Scene.func_array
     del bpy.types.Scene.active_func_array_index
     del bpy.types.Object.is_func_array_dummy
+
+    if deform_update in bpy.app.handlers.depsgraph_update_post:
+        bpy.app.handlers.depsgraph_update_post.remove(deform_update)
