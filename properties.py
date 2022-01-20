ea3d74343d47507598cc05a6ae01d805bbe7e82d
phellutone <phellutone@gmail.com>
2022-01-21 05:21:05 +0900

    Fix controller update

--- properties.py
+++ properties.py
@@ -38,22 +38,19 @@ class FuncArray(bpy.types.PropertyGroup):
         soft_max=25
     )
 
+    def controller_update(self, context: bpy.types.Context) -> None:
+        if self.ctr_min < self.controller and self.ctr_max < self.controller:
+            self.controller = self.ctr_max
+        if self.controller < self.ctr_min and self.controller < self.ctr_max:
+            self.controller = self.ctr_min
     controller: bpy.props.FloatProperty(
-        min=0.0,
-        max=1.0
+        update=controller_update
     )
-    def ctr_update(self, context):
-        self.__class__.controller = bpy.props.FloatProperty(
-            min=self.ctr_min,
-            max=self.ctr_max
-        )
     ctr_min: bpy.props.FloatProperty(
-        default=0.0,
-        update=ctr_update
+        default=0.0
     )
     ctr_max: bpy.props.FloatProperty(
-        default=1.0,
-        update=ctr_update
+        default=1.0
     )
 
     trg_co: bpy.props.PointerProperty(type=bpy.types.Collection)
